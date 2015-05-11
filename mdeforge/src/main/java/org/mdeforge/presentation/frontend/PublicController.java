24f375b817bde40b889fac687ee591a822cb1f57
Juri Di Rocco <juri.dirocco@univaq.it>
2015-05-11 17:05:24 +0200

    Download file, Graphical arrangement

--- mdeforge/src/main/java/org/mdeforge/presentation/frontend/PublicController.java
+++ mdeforge/src/main/java/org/mdeforge/presentation/frontend/PublicController.java
@@ -1,8 +1,12 @@
 package org.mdeforge.presentation.frontend;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.mdeforge.business.ContainmentRelationService;
 import org.mdeforge.business.CosineSimilarityRelationService;
@@ -26,7 +30,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/public")
 public class PublicController {
-	
 
 	@Autowired
 	private EcoreMetamodelService ecoreMetamodelService;
@@ -42,80 +45,90 @@ public class PublicController {
 	private GridFileMediaService gridFileMediaService;
 	@Autowired
 	private UserService userService;
-	
-	
-	
+
 	@RequestMapping(value = "/", method = { RequestMethod.GET })
-	public String index() {			
+	public String index() {
 		return "public.index";
 	}
-	
+
 	@RequestMapping(value = "/browse", method = { RequestMethod.GET })
-	public String dashboard(Model model) {			
-		
-		List<EcoreMetamodel> ecoreMetamodelsList = ecoreMetamodelService.findAll();		
+	public String dashboard(Model model) {
+
+		List<EcoreMetamodel> ecoreMetamodelsList = ecoreMetamodelService
+				.findAll();
 		model.addAttribute("ecoreMetamodelsList", ecoreMetamodelsList);
-		
 		List<User> users = userService.findAll();
 		model.addAttribute("users", users);
-		
 		return "public.browse.dashboard";
 	}
-	
+
 	@RequestMapping(value = "/browse/metamodel_details", method = { RequestMethod.GET })
-	public String metamodelDetails(Model model, @RequestParam String metamodel_id) {	
-		
-		EcoreMetamodel ecoreMetamodel = ecoreMetamodelService.findOne(metamodel_id);
+	public String metamodelDetails(Model model,
+			@RequestParam String metamodel_id) {
+
+		EcoreMetamodel ecoreMetamodel = ecoreMetamodelService
+				.findOnePublic(metamodel_id);
 		model.addAttribute("ecoreMetamodel", ecoreMetamodel);
-		
-		
-		String pathToDownload = gridFileMediaService.getFilePath(ecoreMetamodel);
-		
-		
+		String pathToDownload = gridFileMediaService
+				.getFilePath(ecoreMetamodel);
 		File ecoreMetamodelFile = new File(pathToDownload);
 		model.addAttribute("ecoreMetamodelFile", ecoreMetamodelFile);
 
-		List<Metric> metrics = ecoreMetamodelService.getMetrics(ecoreMetamodel);
-		model.addAttribute("metrics", metrics);
-		
-		String serializedContext = ecoreMetamodelService.serializeContent(ecoreMetamodel);		
-		model.addAttribute("serializedContext", serializedContext);
-		
 		return "public.browse.metamodel_details";
 	}
-	
+
+	@RequestMapping(value = "/browse/metamodel_download", method = RequestMethod.GET)
+	public void downloadMetamodel(@RequestParam String metamodel_id,
+			HttpServletResponse response) throws IOException {
+
+		EcoreMetamodel ecoreMetamodel = ecoreMetamodelService
+				.findOne(metamodel_id);
+		InputStream is = gridFileMediaService
+				.getFileInputStream(ecoreMetamodel);
+
+		response.setContentType("application/force-download");
+		response.setHeader("Content-Disposition", "attachment; filename="
+				+ ecoreMetamodel.getName() + ".ecore");
+		// copy it to response's OutputStream
+		org.apache.commons.io.IOUtils.copy(is, response.getOutputStream());
+		response.flushBuffer();
+	}
+
 	@RequestMapping(value = "/browse/metamodels_list", method = { RequestMethod.GET })
-	public String metamodelsList(Model model) {					
-		
-		List<EcoreMetamodel> ecoreMetamodelsList = ecoreMetamodelService.findAll();		
+	public String metamodelsList(Model model) {
+
+		List<EcoreMetamodel> ecoreMetamodelsList = ecoreMetamodelService
+				.findAll();
 		model.addAttribute("ecoreMetamodelsList", ecoreMetamodelsList);
-		
+
 		return "public.browse.metamodels_list";
 	}
-	
-	@RequestMapping(value = "/browse/cluster", method = { RequestMethod.GET })
-	public String cluster(Model model, 
-			@RequestParam (value="threshold", required=true, defaultValue="0.3") Double threshold, 
-			@RequestParam (value="computation", required=true, defaultValue="1") int computation
-		) {
-		
 
+	@RequestMapping(value = "/browse/cluster", method = { RequestMethod.GET })
+	public String cluster(
+			Model model,
+			@RequestParam(value = "threshold", required = true, defaultValue = "0.3") Double threshold,
+			@RequestParam(value = "computation", required = true, defaultValue = "1") int computation) {
 		/*
 		 * TABLE
 		 */
 		List<Cluster> clusters = new ArrayList<Cluster>();
 		switch (computation) {
 		case 1:
-			clusters = ecoreMetamodelService.getSimilarityClusters(threshold, similarityRelationService);
+			clusters = ecoreMetamodelService.getSimilarityClusters(threshold,
+					similarityRelationService);
 			break;
 		case 2:
-			clusters = ecoreMetamodelService.getSimilarityClusters(threshold, containmentRelationService);
+			clusters = ecoreMetamodelService.getSimilarityClusters(threshold,
+					containmentRelationService);
 			break;
 		case 3:
-			clusters = ecoreMetamodelService.getSimilarityClusters(threshold, cosineSimilarityRelationService);	
-			break;		
+			clusters = ecoreMetamodelService.getSimilarityClusters(threshold,
+					cosineSimilarityRelationService);
+			break;
 		case 4:
-			clusters = ecoreMetamodelService.getSimilarityClusters(threshold, diceSimilarityRelationService);
+			clusters = ecoreMetamodelService.getSimilarityClusters(threshold,
+					diceSimilarityRelationService);
 			break;
 		}
 		int maxCluster = 0;
@@ -123,94 +136,53 @@ public class PublicController {
 		int count = 0;
 		int noCluster = 0;
 		for (Cluster cluster : clusters) {
-			maxCluster = (maxCluster < cluster.getArtifacts().size())?cluster.getArtifacts().size():maxCluster;
+			maxCluster = (maxCluster < cluster.getArtifacts().size()) ? cluster
+					.getArtifacts().size() : maxCluster;
 			count += cluster.getArtifacts().size();
-			if(cluster.getArtifacts().size()==1){
+			if (cluster.getArtifacts().size() == 1) {
 				noCluster++;
 			}
 		}
-		average =  (count*1.0)/(clusters.size()*1.0);
+		average = (count * 1.0) / (clusters.size() * 1.0);
 		model.addAttribute("clusters", clusters);
 		model.addAttribute("average", average);
 		model.addAttribute("max", maxCluster);
 		model.addAttribute("noCluster", noCluster);
-		
-		
-		//Mettiamo anche le informazioni relative alla Threshold e Computation
+		// Mettiamo anche le informazioni relative alla Threshold e Computation
 		model.addAttribute("threshold", threshold);
 		model.addAttribute("computation", computation);
-		
-		
 		return "public.browse.cluster";
 	}
-	
+
 	@RequestMapping(value = "/browse/cluster_graph", method = { RequestMethod.GET })
-	public String test(Model model, 
-			@RequestParam (value="threshold", required=true, defaultValue="0.3") Double threshold, 
-			@RequestParam (value="computation", required=true, defaultValue="1") int computation
-		) {
-		
+	public String test(
+			Model model,
+			@RequestParam(value = "threshold", required = true, defaultValue = "0.3") Double threshold,
+			@RequestParam(value = "computation", required = true, defaultValue = "1") int computation) {
+
 		/*
 		 * GRAPH
 		 */
 		String graph = null;
 		switch (computation) {
 		case 1:
-			graph = ecoreMetamodelService.getSimilarityGraph(threshold, similarityRelationService);
+			graph = ecoreMetamodelService.getSimilarityGraph(threshold,
+					similarityRelationService);
 			break;
 		case 2:
-			graph = ecoreMetamodelService.getSimilarityGraph(threshold, containmentRelationService);
+			graph = ecoreMetamodelService.getSimilarityGraph(threshold,
+					containmentRelationService);
 			break;
 		case 3:
-			graph = ecoreMetamodelService.getSimilarityGraph(threshold, cosineSimilarityRelationService);	
-			break;		
+			graph = ecoreMetamodelService.getSimilarityGraph(threshold,
+					cosineSimilarityRelationService);
+			break;
 		case 4:
-			graph = ecoreMetamodelService.getSimilarityGraph(threshold, diceSimilarityRelationService);
+			graph = ecoreMetamodelService.getSimilarityGraph(threshold,
+					diceSimilarityRelationService);
 			break;
 		}
 		model.addAttribute("graph", graph);
-		
-		/*
-		 * TABLE
-		 */
-		List<Cluster> clusters = new ArrayList<Cluster>();
-		switch (computation) {
-		case 1:
-			clusters = ecoreMetamodelService.getSimilarityClusters(threshold, similarityRelationService);
-			break;
-		case 2:
-			clusters = ecoreMetamodelService.getSimilarityClusters(threshold, containmentRelationService);
-			break;
-		case 3:
-			clusters = ecoreMetamodelService.getSimilarityClusters(threshold, cosineSimilarityRelationService);	
-			break;		
-		case 4:
-			clusters = ecoreMetamodelService.getSimilarityClusters(threshold, diceSimilarityRelationService);
-			break;
-		}
-		
-		int maxCluster = 0;
-		double average = 0;
-		int count = 0;
-		int noCluster = 0;
-		for (Cluster cluster : clusters) {
-			maxCluster = (maxCluster < cluster.getArtifacts().size())?cluster.getArtifacts().size():maxCluster;
-			count += cluster.getArtifacts().size();
-			if(cluster.getArtifacts().size()==1){
-				noCluster++;
-			}
-		}
-		average =  (count*1.0)/(clusters.size()*1.0);
-		model.addAttribute("clusters", clusters);
-		model.addAttribute("average", average);
-		model.addAttribute("max", maxCluster);
-		model.addAttribute("noCluster", noCluster);
-		
-		//Mettiamo anche le informazioni relative alla Threshold e Computation
-		model.addAttribute("threshold", threshold);
-		model.addAttribute("computation", computation);
-				
-		
 		return "public.browse.cluster.graph";
 	}
 }
