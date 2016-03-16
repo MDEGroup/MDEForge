2ee75be756bb8aefd7f0ab6100f2fa7334ea9758
md2manoppello <juri.di.rocco@gmail.com>
2016-03-16 10:46:36 +0100

    Add create index java stand alone

---
+++ mdeforge/src/main/java/org/mdeforge/importer/CreateIndex.java
@@ -0,0 +1,15 @@
+package org.mdeforge.importer;
+
+import org.mdeforge.business.EcoreMetamodelService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class CreateIndex {
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"file:src/main/webapp/WEB-INF/spring/root-context.xml");
+		EcoreMetamodelService ec = context
+				.getBean(EcoreMetamodelService.class);
+		ec.createIndex();
+	}
+}
