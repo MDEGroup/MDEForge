6501b41c6685aa15ecd8d0c6069e5433523981e9
Di Rocco Juri <Di Rocco Juri>
2016-04-19 17:24:53 +0200

    fix bugs

--- mdeforge/src/main/java/org/mdeforge/importer/CreareEcoreSimilarityRelation.java
+++ mdeforge/src/main/java/org/mdeforge/importer/CreareEcoreSimilarityRelation.java
@@ -18,10 +18,11 @@ public class CreareEcoreSimilarityRelation {
 		List<EcoreMetamodel> ecoreMMlist = ecoreMetamodelService.findAll();
 		EcoreMetamodel [] ecoreMMArray = ecoreMMlist.toArray(new EcoreMetamodel[ecoreMMlist.size()]);
 		System.out.println("start time: " + new Date());
-		for (int i =0; i < ecoreMMArray.length-1; i++) {
+		for (int i = 0; i < ecoreMMArray.length-1; i++) {
 			System.out.println(ecoreMMArray[i].getName() + " " + i + " of "+ (ecoreMMlist.size()-i) + " start time: " + new Date());
 			for (int j = i+1; j <ecoreMMArray.length; j++) {
 				try {
+					System.out.println(ecoreMMArray[j].getName());
 					if(j % 100 == 0)
 						System.out.println("Coputed " + j + " of " + (ecoreMMlist.size()-i));
 					ecoreMetamodelService.calculateSimilarity(ecoreMMArray[i], ecoreMMArray[j]);
