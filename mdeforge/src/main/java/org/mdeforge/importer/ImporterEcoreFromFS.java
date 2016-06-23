c9cc3be5f8c68a0ae3fc25fbf29bd2aa03d80816
Di Rocco Juri <Di Rocco Juri>
2016-06-23 17:02:01 +0200

    Restore importer

--- mdeforge/src/main/java/org/mdeforge/importer/ImporterEcoreFromFS.java
+++ mdeforge/src/main/java/org/mdeforge/importer/ImporterEcoreFromFS.java
@@ -2,25 +2,12 @@ package org.mdeforge.importer;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
 
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.store.Directory;
-import org.apache.lucene.store.FSDirectory;
-import org.eclipse.core.resources.mapping.ModelStatus;
-import org.eclipse.egit.github.core.RepositoryContents;
-import org.eclipse.emf.ecore.resource.Resource;
 import org.mdeforge.business.EcoreMetamodelService;
 import org.mdeforge.business.GridFileMediaService;
-import org.mdeforge.business.ModelService;
-import org.mdeforge.business.impl.ModelServiceImpl;
 import org.mdeforge.business.model.ConformToRelation;
 import org.mdeforge.business.model.EcoreMetamodel;
-import org.mdeforge.business.model.GridFileMedia;
 import org.mdeforge.business.model.Model;
 import org.mdeforge.business.model.Property;
 import org.mdeforge.business.model.User;
@@ -67,17 +54,36 @@ public class ImporterEcoreFromFS {
 	
 	public static void main(String[] args) {
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("file:src/main/webapp/WEB-INF/spring/root-context.xml");
-		EcoreMetamodelService modelService = context.getBean(EcoreMetamodelService.class);
-		ModelService mmodelService = context.getBean(ModelService.class);
-		EcoreMetamodel emm = modelService.findOne("575ecc30d4c6c76c196888d8");
-		Resource r = modelService.loadArtifact(emm);
-		r.getContents().forEach(x->System.out.println("JURI "+x));
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"file:src/main/webapp/WEB-INF/spring/root-context.xml");
+		GridFileMediaService gfms = context.getBean(GridFileMediaService.class);
+		EcoreMetamodelService ecoreMetamodelService = context.getBean(EcoreMetamodelService.class);
+		RepositoryContentsRepository repositoryContentsRepository = context.getBean(RepositoryContentsRepository.class);
+		User usr = new User();
+		usr.setId("5514b943d4c6c379396fe8b7");
+		File folder = new File("githubmetamodels");
+		File[] listOfFiles = folder.listFiles();
+		for (File file : listOfFiles) {
+			if (file.toString().endsWith(".ecore")) {
+		        try {
+		        	EcoreMetamodel emm = new EcoreMetamodel();
+		        	emm.setName(file.getName());
+		        	emm.setAuthor(usr);
+		        	emm.setFile(gfms.createObjectFromFile(file.toString(), file.getName().toString()));
+		        	emm.setOpen(true);
+		        	Property p = new Property();
+		        	p.setName("imported");
+		        	p.setValue("github");
+		        	ecoreMetamodelService.create(emm);
+		        	System.out.println(file.getName().toString());
+		        } catch (Exception e) {
+		        	System.out.println("ERROR: " + file);
+		        }
+		    }
+		}
+			
 		
-		
-		
-		Model testModel = mmodelService.findOne("575eb7fdd4c60e5eef616060");
-		mmodelService.createIndex(testModel);
+		((ConfigurableApplicationContext)context).close();
 		
 	}
 }
