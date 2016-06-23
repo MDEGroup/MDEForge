97eff85bc82927b3a4865b54bebf493e80f8d74e
Francesco Basciani <francesco.basciani@gmail.com>
2016-06-23 11:45:25 +0200

    Lucene Tags detecting first approach

---
+++ mdeforge/src/main/java/org/mdeforge/business/model/LuceneTag.java
@@ -0,0 +1,23 @@
+package org.mdeforge.business.model;
+
+public class LuceneTag {
+	
+	private String tagName;
+	private String artifactKindValue;
+	
+	public String getTagName() {
+		return tagName;
+	}
+	public void setTagName(String tagName) {
+		this.tagName = tagName;
+	}
+	public String getArtifactKindValue() {
+		return artifactKindValue;
+	}
+	public void setArtifactKindValue(String artifactKindValue) {
+		this.artifactKindValue = artifactKindValue;
+	}
+	
+	
+
+}
