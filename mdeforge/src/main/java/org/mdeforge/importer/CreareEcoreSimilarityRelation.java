ec8f0f3cd6631ed171b5d49ad8162c0a35000320
md2manoppello <juri.di.rocco@gmail.com>
2017-05-03 16:53:51 +0200

    added a new method to calculate similarity matrix

--- mdeforge/src/main/java/org/mdeforge/importer/CreareEcoreSimilarityRelation.java
+++ mdeforge/src/main/java/org/mdeforge/importer/CreareEcoreSimilarityRelation.java
@@ -1,37 +1,71 @@
 package org.mdeforge.importer;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.Date;
 import java.util.List;
 
+import org.mdeforge.business.BusinessException;
 import org.mdeforge.business.EcoreMetamodelService;
 import org.mdeforge.business.model.EcoreMetamodel;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.security.crypto.codec.Base64;
 public class CreareEcoreSimilarityRelation {
 	public static void main(String[] args) {
+		
+		String basePath = "/home/juri/development/forgeDir/";
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"file:src/main/webapp/WEB-INF/spring/root-context.xml");
 		EcoreMetamodelService ecoreMetamodelService = context.getBean(EcoreMetamodelService.class);
 //		RelationRepository rr = context.getBean(RelationRepository.class);
 		List<EcoreMetamodel> ecoreMMlist = ecoreMetamodelService.findAll();
 		EcoreMetamodel [] ecoreMMArray = ecoreMMlist.toArray(new EcoreMetamodel[ecoreMMlist.size()]);
-		System.out.println("start time: " + new Date());
+		write("start time: " + new Date());
 		for (int i = 0; i < ecoreMMArray.length-1; i++) {
-			System.out.println(ecoreMMArray[i].getName() + " " + i + " of "+ (ecoreMMlist.size()-i) + " start time: " + new Date());
+			write(ecoreMMArray[i].getName() + " " + i + " of "+ (ecoreMMlist.size()-i) + " start time: " + new Date());
 			for (int j = i+1; j <ecoreMMArray.length; j++) {
 				try {
-					System.out.println(ecoreMMArray[j].getName());
 					if(j % 100 == 0)
-						System.out.println("Coputed " + j + " of " + (ecoreMMlist.size()-i));
+						write("Coputed " + j + " of " + (ecoreMMlist.size()-i));
 					ecoreMetamodelService.calculateSimilarity(ecoreMMArray[i], ecoreMMArray[j]);
 				} catch (Exception e) {
-					System.err.println("ERROR: " + ecoreMMArray[i].getName() + " " + i  + " _ " + ecoreMMArray[j].getName() + " " + j);
+					write("ERROR: " + ecoreMMArray[i].getName() + " " + i  + " _ " + ecoreMMArray[j].getName() + " " + j);
 				}
 			}
 		}
-		System.out.println("end time: " + new Date());
+		write("end time: " + new Date());
 		((ConfigurableApplicationContext)context).close();
+	}
+	
+	public static void write(String s) {
+		String basePath = "/home/juri/development/forgeDir/";
+		FileSystemResource resource = new FileSystemResource(basePath + "report.txt");
+		File f = resource.getFile();
+		BufferedWriter bw = null;
+		FileWriter fw = null;
+
+		try {
+			fw = new FileWriter(f.getAbsoluteFile(), true);
+			bw = new BufferedWriter(fw);
+			bw.write(s + "\n");
+		}catch (FileNotFoundException e) {} catch (IOException e) {} catch (Exception e) {}
+		finally {
+			try {
+				if (bw != null)
+					bw.close();
+				if (fw != null)
+					fw.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
 	}
 }
