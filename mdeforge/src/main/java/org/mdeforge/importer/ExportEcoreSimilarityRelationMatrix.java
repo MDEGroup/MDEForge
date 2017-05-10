d9208ac8ad0623e4ec431debcac9004d1badd9da
md2manoppello <juri.di.rocco@gmail.com>
2017-05-10 11:15:38 +0200

    Utility to export similarity matrix

--- mdeforge/src/main/java/org/mdeforge/importer/ExportEcoreSimilarityRelationMatrix.java
+++ mdeforge/src/main/java/org/mdeforge/importer/ExportEcoreSimilarityRelationMatrix.java
@@ -1,19 +1,22 @@
 package org.mdeforge.importer;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Date;
 import java.util.List;
 
 import org.mdeforge.business.EcoreMetamodelService;
-import org.mdeforge.business.SemanticSimilarityRelationService;
+import org.mdeforge.business.SimilarityRelationService;
 //import org.mdeforge.business.SemanticSimilarityRelationServiceV1;
 import org.mdeforge.business.model.EcoreMetamodel;
-import org.mdeforge.business.model.SemanticSimilarityRelation;
+import org.mdeforge.business.model.SimilarityRelation;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.FileSystemResource;
 
 public class ExportEcoreSimilarityRelationMatrix {
 	public static void main(String[] args) throws IOException {
@@ -22,40 +25,64 @@ public class ExportEcoreSimilarityRelationMatrix {
 		EcoreMetamodelService ecoreMetamodelService = context.getBean(EcoreMetamodelService.class);
 //		MetricRepository mr = context.getBean(MetricRepository.class);
 		// RelationRepository rr = context.getBean(RelationRepository.class);
-		SemanticSimilarityRelationService sr = context.getBean(SemanticSimilarityRelationService.class);
-		File f = new File("C:\\Users\\juri\\development\\forgeDir\\AAA.txt");
-		FileWriter fw = new FileWriter(f);
+		SimilarityRelationService sr = context.getBean(SimilarityRelationService.class);
+		
+		//CHECK FILE
 		List<EcoreMetamodel> ecoreMMlist = ecoreMetamodelService.findAll();
 		EcoreMetamodel[] ecoreMMArray = ecoreMMlist.toArray(new EcoreMetamodel[ecoreMMlist.size()]);
 		System.out.println("start time: " + new Date());
+		String s = "";
 		for (int i = 0; i < ecoreMMArray.length - 1; i++) {
-			fw.write(ecoreMMArray[i].getName() + ";");
+			s = s + ecoreMMArray[i].getName() + ";";
 		}
-		fw.write(System.lineSeparator());
+		write(s);
+		s = "";
 		for (int i = 0; i < ecoreMMArray.length - 1; i++) {
 			System.out.println(ecoreMMArray[i].getName() + " " + i + " of " + (ecoreMMlist.size() - i) + " start time: "
 					+ new Date());
 			for (int j = 0; j < ecoreMMArray.length; j++) {
 
 				if (ecoreMMArray[i].equals(ecoreMMArray[j])) {
-					fw.write("1.0;");
+					s += "1.0;";
 				} else {
-					SemanticSimilarityRelation smv = sr.findOneByArtifacts(ecoreMMArray[i], ecoreMMArray[j]);
+					SimilarityRelation smv = sr.findOneByArtifacts(ecoreMMArray[i], ecoreMMArray[j]);
 					if (smv == null) {
 						smv = sr.findOneByArtifacts(ecoreMMArray[j], ecoreMMArray[i]);
 						if (smv == null)
-							fw.write("NAN;");
+							s += "NAN;";
 						else
-							fw.write(smv.getValue() + ";");
+							s += smv.getValue() + ";";
 					} else
-						fw.write(smv.getValue() + ";");
+						s += smv.getValue() + ";";
 				}
-
 			}
-			fw.write(System.lineSeparator());
+			s += System.lineSeparator();
 		}
-		fw.close();
+		
 		System.out.println("end time: " + new Date());
 		((ConfigurableApplicationContext) context).close();
+	}
+	public static void write(String s) {
+		String basePath = "/home/mdeforge/";
+		FileSystemResource resource = new FileSystemResource(basePath + "report.txt");
+		File f = resource.getFile();
+		BufferedWriter bw = null;
+		FileWriter fw = null;
+
+		try {
+			fw = new FileWriter(f.getAbsoluteFile(), true);
+			bw = new BufferedWriter(fw);
+			bw.write(s);
+		}catch (FileNotFoundException e) {} catch (IOException e) {} catch (Exception e) {}
+		finally {
+			try {
+				if (bw != null)
+					bw.close();
+				if (fw != null)
+					fw.close();
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
 	}
 }
